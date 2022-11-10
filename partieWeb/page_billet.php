65128d775f72a3fd244a8c0856035164e39257e4
5quentinus <quentin.st.flour@gmail.com>
2022-11-10 09:52:45 +0100

    corect

--- partieWeb/page_billet.php
+++ partieWeb/page_billet.php
@@ -14,12 +14,12 @@
 
 <body>
 
-  <?php include './header_footer/header.php';?>
+  <?php include './header_footer/header.php'; ?>
 
   <div class="ticketContainer">
     <div class="desc">
       <h1 id="prems">There are several formulas,<br> here is the list of them:</h1>
-      
+
       <h1 id="stand">Standard Pass</h1>
       <p>55€ : access to all areas except concert</p>
 
@@ -30,38 +30,39 @@
 
       <h1 class="europe" id="europe">Discover and vote for the works of your country <span id="bleu">!</span><br>Events in collaboration with Culturo <br>will be fixed at <span id="orange">50€</span></h1>
 
-      
+
 
     </div>
 
     <div class="container_billet">
-
-    <h1>Reservation part :</h1>
-      <form class="billet" method="POST" action="page_billet.php" enctype="multipart/form-data"> 
+      <h1>Reservation part :</h1>
+      <form class="billet" method="POST" action="page_billet.php" enctype="multipart/form-data">
         <img src="./images/logo.png">
 
         <div class="centre">
 
-          <div class="titre"><h2>What formula do you want ? :</h2></div>
+          <div class="titre">
+            <h2>What formula do you want ? :</h2>
+          </div>
 
           <p>
             <select id="billet" name="type_billet">
               <?php
               include "./include/connexionBDD.php";
               include "./POO/typeBillets.php";
-              
+
               $BDDCo = new connexionBDD();
 
               $afficher = (array)$BDDCo;
               $afficher2  = $afficher['tab_typeBillet'];
 
-              for ($i=0 ; $i<count($afficher2) ; $i++){
-                echo("<option value='".$afficher2[$i]['lib']."'>  ".$afficher2[$i]['lib']." (".$afficher2[$i]['prix']."€)</option>");
+              for ($i = 0; $i < count($afficher2); $i++) {
+                echo ("<option value='" . $afficher2[$i]['lib'] . "'>  " . $afficher2[$i]['lib'] . " (" . $afficher2[$i]['prix'] . "€)</option>");
               }
               ?>
             </select>
           </p>
-              
+
           <p class="half">
             <input type="text" name="nom" placeholder="Name..." required>
             <input type="text" name="pnom" placeholder="First Name..." required>
@@ -74,58 +75,53 @@
 
           <?php
 
-            include_once('./fonction.php');
-            include "./POO/Billets.php";
-                
-            $file ="./sauv.txt";
+          include_once('./fonction.php');
+          include "./POO/Billets.php";
 
-            if (file_exists($file)!=false){
-              $file ="./sauv.txt";
-              $read=file($file);
+          $file = "./sauv.txt";
 
-              if($read[0]!="" && $read[0]!="null"){
-                $idCo = $read[0];
-              }
+          if (file_exists($file) != false) {
+            $file = "./sauv.txt";
+            $read = file($file);
 
-              else {
-                echo "<script>alert('Your not connected')</script>";
-                echo '<script>document.location.href="connexion.php"</script>';
-              }
-            }
-
-            else {
+            if ($read[0] != "" && $read[0] != "null") {
+              $idCo = $read[0];
+            } else {
               echo "<script>alert('Your not connected')</script>";
               echo '<script>document.location.href="connexion.php"</script>';
             }
-          
-          if(isset($_POST["reserve"])){
+          } else {
+            echo "<script>alert('Your not connected')</script>";
+            echo '<script>document.location.href="connexion.php"</script>';
+          }
+
+          if (isset($_POST["reserve"])) {
             include "./POO/Billets.php";
 
-            if(isset($_POST["concert"])!=null){
-              $select=$_POST["concert"];
-            }
-            else{
-              $select="Non";
+            if (isset($_POST["concert"]) != null) {
+              $select = $_POST["concert"];
+            } else {
+              $select = "Non";
             }
 
-            $billet = new Billets($_POST["nom"],$_POST["pnom"],$idCo,$select, (array)$BDDCo);
+            $billet = new Billets($_POST["nom"], $_POST["pnom"], $idCo, $select, (array)$BDDCo);
             $billet->EnsembleBillets((array)$billet, $_POST["type_billet"]);
             $billet->EnregistrementBDD_Billet((array)$billet);
           }
           ?>
         </div>
-        
+
         <div class="boutton">
           <input type="submit" name="reserve" value="Reserve">
         </div>
-        
+
       </form>
     </div>
 
   </div>
 
-    <?php include './header_footer/footer.html';?>
+  <?php include './header_footer/footer.html'; ?>
 
-    
-    <script type="text/javascript" src="./js/scriptVilles.js"></script>
+
+  <script type="text/javascript" src="./js/scriptVilles.js"></script>
 </body>
